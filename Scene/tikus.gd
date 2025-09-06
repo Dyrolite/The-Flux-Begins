5e2593a2fec826897a93221335b5561634a9b74b
Nafis <fakhrynafiss@gmail.com>
2025-09-06 07:52:25 +0700

    menambahkan gambar tikus dan membetulkan movement

--- Scene/tikus.gd
+++ Scene/tikus.gd
@@ -3,27 +3,25 @@ extends CharacterBody2D
 class_name Player
 
 @export var speed = 300
+
 var movement_direction = Vector2.ZERO
-
-
 
 func _physics_process(delta):
 	get_input()
-	
 	velocity = movement_direction * speed
 	move_and_slide()
 
-
 func get_input():
-	if Input.is_action_pressed("Left"):
-		movement_direction = Vector2.LEFT
-		rotation_degrees = 0
-	elif Input.is_action_pressed("Right"):
+	if Input.is_action_pressed("Right"):
 		movement_direction = Vector2.RIGHT
-		rotation_degrees = 180
+		scale.x = 1 
+		
+	elif Input.is_action_pressed("Left"):
+		movement_direction = Vector2.LEFT
+		scale.x = -1 
+		
 	elif Input.is_action_pressed("Down"):
 		movement_direction = Vector2.DOWN
-		rotation_degrees = 270
+		
 	elif Input.is_action_pressed("Up"):
 		movement_direction = Vector2.UP
-		rotation_degrees = 90
