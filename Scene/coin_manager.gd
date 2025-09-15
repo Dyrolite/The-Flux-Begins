98664d1875573f54d1eb572f5015595da70b8e8d
GavinArdhijaya91 <gavin.ardhijaya@gmail.com>
2025-09-15 21:08:30 +0700

    Create coin_manager.gd

---
+++ Scene/coin_manager.gd
@@ -0,0 +1,34 @@
+extends Node
+
+var total_pellets_count
+var pellets_eaten = 0
+
+@onready var point_manager: PointManager = $"../point manager"
+@onready var ui: CanvasLayer = $"../UI" as UI
+# Gunakan tipe data 'Mahasiswa' yang spesifik
+@export var mahasiswa_array : Array[Mahasiswa]
+@export var player: Player
+
+func _ready():
+	var pellets = self.get_children() as Array[Coin]
+	print("Jumlah koin yang ditemukan oleh manager: ", pellets.size())
+	total_pellets_count = pellets.size()
+	
+	for pellet in pellets:
+		pellet.koin_diambil.connect(on_pellet_eaten)
+
+func on_pellet_eaten(bisa_kalahkan_mahasiswa: bool):
+	pellets_eaten += 1
+	if bisa_kalahkan_mahasiswa:
+		get_tree().create_timer(8).timeout.connect(reset_point_for_mahasiswa)
+		for mahasiswa in mahasiswa_array:
+			# Sekarang Godot tahu bahwa 'mahasiswa' memiliki fungsi lari_dari_tikus()
+			mahasiswa.lari_dari_tikus()
+	print("Koin dimakan: ", pellets_eaten, " / ", total_pellets_count)
+	if pellets_eaten == total_pellets_count:
+		ui.game_won()
+		if player: # Pengecekan keamanan
+			player.win()
+
+func reset_point_for_mahasiswa():
+	point_manager.reset_point_for_mahasiswa()
